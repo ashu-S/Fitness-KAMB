2f2975d65a908ee7af1679a38cf4a60155a06645
bonnieho <bonnieho@rice.edu>
2017-08-05 22:33:10 -0500

    IT WORKS - correctly maps restaurants using user's geolocation

--- API_test_02/zomato-googleAPI.js
+++ API_test_02/zomato-googleAPI.js
@@ -1,126 +1,99 @@
-// <!-- Google places Maps script here -->
-    // <!-- <script> -->
-var data;
+
+
 var map;
-var infoWindow;
-var request;
-var service;
+var infoWindow; // use later for pop up info windows on restaurants
 var markers=[];
 
-// Zomato Script here
-    // <script type="application/javascript">
-    function onload(param) {
-        Zomato.init({
-            key: "c8e2f5b03a41606edd2c1b651d82edec"
-        });
-     // ***** THIS IS THE ONLY FUNCTION YOU NEED TO CALL!!!
-        Zomato.search({
-            latitude: 29.779043,
-            longitude: -95.5450048
-        }, [143, 164, 998, 308], function(s) {
-            // document.getElementById("geocode_op").innerHTML = JSON.stringify(s);
-            console.log(s);
-            data = s;
-            // console.log(data.restaurants[3].restaurant.location.latitude,data.restaurants[3].restaurant.location.longitude);
-            // invoke Google API pass response as a parameter
+// loading page populating the map
+function onload(param) {
 
-            initialize(s);
+  // this ccnstant defaults to downtown Houston (used if user denies location services)
+  const DEFAULT_POS = {
+    latitude: 29.7632800,
+    longitude: -95.3632700
+  };
+  var mypos = DEFAULT_POS;
 
-
-               }, function(e) {
-            // document.getElementById("geocode_op").innerHTML = "*******ERROR******" + JSON.stringify(e);
-            console.log(e)
-
-
-        });
-    }
-
-
-
-     //-------- Here I'm incorporating the Google API code--------
-    function initialize(res) {
-        console.log("in initialize")
-        console.log(res);
-        //console.log(data.restaurants[].restaurant.location.latitude,data.restaurants[].restaurant.location.longitude);  
-        // console.log(data.restaurants[3].restaurant.location.latitude,data.restaurants[3].restaurant.location.longitude);  
-        var center = new google.maps.LatLng(res.restaurants[3].restaurant.location.latitude,res.restaurants[3].restaurant.location.longitude);
-
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: center,
-          zoom: 13
-        });
-
-        callback(res)
-
-/*
-     request = {
-        location: center,
-        radius:8047,  //5 miles in meters
-        types: ['restaurant']  ///can use for various places
-      };
-
-      infoWindow = new google.maps.InfoWindow();
-
-      service = new google.maps.places.PlacesService(map);
-
-      service.nearbySearch(request, callback);
-
-      google.maps.event.addListener(map, 'rightclick', function(event){
-        map.setCenter(event.latLng)
-        clearResults(markers)
-
-        var request = {
-          location: event.latLng,
-          radius: 8047,
-          types: ['restaurant']
-        };
-
-        service.nearbySearch(request, callback); 
-
-      }) */
-    }
-
-    function callback(results,status){
-
-      //if (status == google.maps.places.PlacesServiceStatus.OK){
-        for (var i=0; i< results.restaurants.length;i++){
-          markers.push(createMarker(results.restaurants[i]));
-          }
-      //}
-    }
-
-    // place is a Zomato object
-    function createMarker(place){
-      var placeLoc = place.restaurant.location;
-      var marker = new google.maps.Marker({
-        map: map,
-        // converting from Zomato format (string) to Google format (floating point)
-        position: {lat: parseFloat(placeLoc.latitude), lng: parseFloat(placeLoc.longitude)}
-      });
-    
-    google.maps.event.addListener(marker, 'click', function(){
-        infoWindow.setContent(place.name);
-        // infoWindow.setContent(place.rating);
-        infoWindow.open(map,this);
+  // Try HTML5 geolocation
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      // updating mypos with user's geolocation
+      mypos.latitude = position.coords.latitude;
+      mypos.longitude = position.coords.longitude;
+      // create and draw map with restaurants near user's location
+      mapRestaurants(mypos);
+    }, function() {
+      // if geolocation is supported but blocked, draw map using default location
+      handleLocationError(true);
+      mapRestaurants(DEFAULT_POS);
     });
-
-    return marker;
-
-  } 
-  
-  function clearResults(markers){
-    for (var m in markers){
-      markers[m].setMap(null)
-    }
-    markers = []
+  } else {
+    // Browser doesn't support Geolocation draw map using default location
+    handleLocationError(false);
+    mapRestaurants(DEFAULT_POS);
   }
 
-       //google.maps.event.addDomListener(window, 'load', initialize);
-  //----------END of Google API code------------ 
+  function mapRestaurants(pos) {
+    Zomato.init({
+      key: "c8e2f5b03a41606edd2c1b651d82edec"
+    });
+    // find restaurants with cuisine types: healthy food, juices, salads, and vegetarian (by Zomato cuisine code)
+    Zomato.search(pos, [143, 164, 998, 308], function(s) {
+      // invoke Google API pass response as a parameter
+      initialize(pos, s);
+    }, function(e) {
+      // TODO - ERROR CASE - may want to go back and write this to html
+      console.error(e)
+    });        
+  }
+
+  function handleLocationError(browserHasGeolocation) {
+    console.error(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+  }
+}
+// END onLoad function  
 
 
 
-  // </script>    
+//-------- Here I'm incorporating the Google API code to generate a new map --------
+function initialize(pos, res) {
+  var center = new google.maps.LatLng(pos.latitude,pos.longitude);
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: center,
+    zoom: 13
+  });
 
-           
-    // <!-- </script> -->
+  placeMarkers(res);
+}
+
+// This places markers for the restaurants
+function placeMarkers(results) {
+  for (var i=0; i< results.restaurants.length;i++){
+    markers.push(createMarker(results.restaurants[i]));
+  }
+}
+
+// FYI "place" is a Zomato object
+function createMarker(place) {
+  var placeLoc = place.restaurant.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    // converting from Zomato format (string) to Google format (floating point)
+    position: {lat: parseFloat(placeLoc.latitude), lng: parseFloat(placeLoc.longitude)}
+  });
+
+  // TODO - ability to click on new location to get pop up info (add in later)
+
+  return marker;
+} 
+  
+function clearResults(markers){
+  for (var m in markers){
+    markers[m].setMap(null)
+  }
+  markers = []
+}
+//----------END of Google API code------------ 
+
