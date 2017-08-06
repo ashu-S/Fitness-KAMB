c4ca43d3b5c564bd4b0541209fa9185a001864de
Mariela Velez <Mariela@Marielas-MacBook-Air.local>
2017-08-05 23:23:26 -0500

    Added the map onto restaurant page. Needs to be formatted to fit the entire box.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,4 @@
-//var slideIndex = 0;
-//showSlides();
-
-// function showSlides() {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none"; 
-//     }
-//     slideIndex++;
-//     if (slideIndex> slides.length) {slideIndex = 1} 
-//     slides[slideIndex-1].style.display = "block"; 
-//     setTimeout(showSlides, 5000); // Change image every 5seconds
-// }
+//Most Recently Done: Copy and pasted Bonnie's code onto this page to incorporate it into the restaurants page.
 
 $(document).ready(function(){
 
@@ -19,5 +6,6 @@ $(document).ready(function(){
 		event.preventDefault();
 		$('#myModal').modal();
 	});
-	
+
+
 });
